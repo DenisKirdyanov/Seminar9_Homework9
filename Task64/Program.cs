91c1da8ec7d4d945f79a752756fae78777c8d989
Denis Kirdyanov <pilsdenis@yandex.ru>
2022-08-20 21:14:47 +0300

    Задание 64-66

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.WriteLine("Задайте значение n ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте значение m ");
+int m = int.Parse(Console.ReadLine());
+
+string Print(int n1, int m1)
+{
+    string s = "";
+    if (n1 == m1 - 1) return s;
+    return (Print(n1 - 1, m1) + n1).ToString();
+}
+
+Console.WriteLine(Print(n, m));
---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Задайте значение n ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте значение m ");
+int m = int.Parse(Console.ReadLine());
+
+int GetSum(int n1, int m1)
+{
+    if (n1 == m1 - 1) return n1;
+    return (GetSum(n1 - 1, m1) + n1);
+}
+
+Console.WriteLine(GetSum(n, m));
+
